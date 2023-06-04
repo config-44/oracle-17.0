6d48b0ebe1bb2514c6f2eadd8cdacaafb902c513
nerzh <emptystamp@gmail.com>
2023-06-04 17:28:52 +0200

    temp

--- oracle-swift/Package.swift
+++ oracle-swift/Package.swift
@@ -14,7 +14,7 @@ var packageDependencies: [Package.Dependency] = [
     .package(url: "https://github.com/nerzh/swift-extensions-pack-linux", .upToNextMajor(from: "1.0.0")),
     .package(url: "https://github.com/apple/swift-log", .upToNextMajor(from: "1.0.0")),
     .package(url: "https://github.com/GraphQLSwift/GraphQLWS", .upToNextMajor(from: "0.0.1")),
-    .package(url: "https://github.com/apollographql/apollo-ios", .upToNextMajor(from: "1.0.0")),
+    .package(url: "https://github.com/nerzh/apollo-ios", branch: "main"),
 ]
 
 var mainTarget: [Target.Dependency] = [
